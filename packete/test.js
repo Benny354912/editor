32e5d9a5b53c4d3afc0d3829121c17f2112040bc
Benny354912 <82321294+Benny354912@users.noreply.github.com>
2025-06-06 18:06:20 +0200

    Update test.js

--- packete/test.js
+++ packete/test.js
@@ -1,1 +1,7 @@
-alert("Minecraft Test verify");
+// Funktion, die den Alert anzeigt
+function showAlert() {
+    alert("Minecraft");
+}
+
+// Starte den Timer (5000 ms = 5 Sekunden)
+const intervalId = setInterval(showAlert, 5000);
